596048cd2fc4b060805b6d46f3fba237f2b08ff6
5aledmaged <5aledmaged@gmail.com>
2018-08-14 19:13:42 +0200

    add id to mock data

--- src/dev/schema.js
+++ src/dev/schema.js
@@ -1,30 +1,35 @@
 module.exports = {
-    "type": "object",
-    "properties": {
-        "notes": {
-            "type": "array",
-            "minItems": 10,
-            "maxItems": 25,
-            "items": {
-                "type": "object",
-                "properties": {
-                    "title": {
-                        "type": "string"
-                    },
-                    "body": {
-                        "type": "string",
-                        "faker": {
-                            "lorem.paragraph": [10]
-                        }
-                    }
-                },
-                "required": [
-                    "title"
-                ]
+  "type": "object",
+  "properties": {
+    "notes": {
+      "type": "array",
+      "minItems": 10,
+      "maxItems": 25,
+      "items": {
+        "type": "object",
+        "properties": {
+          "id": {
+            "type": "integer",
+            "autoIncrement": true
+          },
+          "title": {
+            "type": "string"
+          },
+          "body": {
+            "type": "string",
+            "faker": {
+              "lorem.paragraph": [10]
             }
-        }
-    },
-    "required": [
-        "notes"
-    ]
+          }
+        },
+        "required": [
+          "id",
+          "title"
+        ]
+      }
+    }
+  },
+  "required": [
+    "notes"
+  ]
 };
