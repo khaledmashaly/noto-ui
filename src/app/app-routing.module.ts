d21f3459ba5682290631e5cb7ff2c21c9a1c9152
5aledmaged <5aledmaged@gmail.com>
2018-08-18 18:38:07 +0200

    setup routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NoteListComponent } from './note-list/note-list.component';
+import { EditNoteComponent } from './edit-note/edit-note.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: NoteListComponent },
+  { path: 'edit-note/:id', component: EditNoteComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
