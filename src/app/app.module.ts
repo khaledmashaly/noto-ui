2835d07a37f5bbe9e70b53c5c447413f43e7b5c3
5aledmaged <5aledmaged@gmail.com>
2018-08-19 06:28:36 +0200

    get note by id and bind it to edit-note component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 import {
   MatToolbarModule,
   MatIconModule,
@@ -29,6 +31,7 @@ import { EditNoteComponent } from './edit-note/edit-note.component';
     BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
--- src/app/edit-note/edit-note.component.ts
+++ src/app/edit-note/edit-note.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Note } from '../shared/note';
+import { NotesService } from '../notes.service';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-edit-note',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditNoteComponent implements OnInit {
 
-  constructor() { }
+  note: Note = {
+    id: 0,
+    title: '',
+    body: ''
+  };
+
+  constructor(
+    private noteService: NotesService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
 
   ngOnInit() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.noteService.getNote(id)
+                    .subscribe(note => this.note = note);
   }
 
 }
