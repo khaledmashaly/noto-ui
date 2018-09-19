1af71be95dbafe0011b8361f43cff722bd5a1abc
5aledmaged <5aledmaged@gmail.com>
2018-09-19 06:28:04 +0200

    update id to use faker.random.uuid

--- src/dev/schema.js
+++ src/dev/schema.js
@@ -1,35 +1,35 @@
 module.exports = {
-  "type": "object",
-  "properties": {
-    "notes": {
-      "type": "array",
-      "minItems": 10,
-      "maxItems": 25,
-      "items": {
-        "type": "object",
-        "properties": {
-          "id": {
-            "type": "integer",
-            "autoIncrement": true
-          },
-          "title": {
-            "type": "string"
-          },
-          "body": {
-            "type": "string",
-            "faker": {
-              "lorem.paragraph": [10]
-            }
-          }
-        },
-        "required": [
-          "id",
-          "title"
-        ]
-      }
-    }
-  },
-  "required": [
-    "notes"
-  ]
+	type: 'object',
+	properties: {
+		notes: {
+			type: 'array',
+			minItems: 10,
+			maxItems: 25,
+			items: {
+				type: 'object',
+				properties: {
+					id: {
+						type: 'string',
+						faker: 'random.uuid'
+					},
+					title: {
+						type: 'string'
+					},
+					body: {
+						type: 'string',
+						faker: {
+							'lorem.paragraph': [10]
+						}
+					}
+				},
+				required: [
+					'id',
+					'title'
+				]
+			}
+		}
+	},
+	required: [
+		'notes'
+	]
 };
