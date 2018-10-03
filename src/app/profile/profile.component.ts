c1fa10a7eb06b020428210db2c0f524f0409c0a1
5aledmaged <5aledmaged@gmail.com>
2018-10-03 18:45:57 +0200

    add user details

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { UserDetails } from '../shared/auth';
 
 @Component({
 	templateUrl: './profile.component.html',
 	styleUrls: ['./profile.component.sass']
 })
 export class ProfileComponent implements OnInit {
+	user: UserDetails;
 
-	constructor() { }
+	constructor(private auth: AuthService) { }
 
 	ngOnInit() {
+		this.auth.profile().subscribe(
+			user => this.user = user,
+			err => console.log(err)
+		);
 	}
 
 }
