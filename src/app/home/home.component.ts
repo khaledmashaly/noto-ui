72644630f8b5939c2e0e819b745af35977cfd172
5aledmaged <5aledmaged@gmail.com>
2018-10-04 19:37:39 +0200

    if user is logged in, auto redirect them to /notes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
-	selector: 'app-home',
 	templateUrl: './home.component.html',
 	styleUrls: ['./home.component.sass']
 })
 export class HomeComponent implements OnInit {
 
-	constructor() { }
+	constructor(private router: Router, private auth: AuthService) { }
 
 	ngOnInit() {
-
+		const isLoggedIn = this.auth.isLoggedIn();
+		if (isLoggedIn) {
+			this.router.navigate(['/notes']);
+		}
 	}
 
 }
