6885b7be633d9d3eecd3bff718d9815f67d6cbe9
5aledmaged <5aledmaged@gmail.com>
2018-10-07 17:16:58 +0200

    auto update header on route change

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { AuthService } from '../auth.service';
 import { UserDetails } from '../shared/auth';
+import { Observable } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
 	selector: 'app-header',
@@ -10,13 +13,28 @@ import { UserDetails } from '../shared/auth';
 export class HeaderComponent implements OnInit {
 	isLoggedIn: boolean;
 	user: UserDetails;
+	navEnd: Observable<NavigationEnd>;
 
-	constructor(private auth: AuthService) { }
+	constructor(private router: Router, private auth: AuthService) {
+		this.navEnd = this.router.events.pipe(
+			filter(event => event instanceof NavigationEnd)
+		) as Observable<NavigationEnd>;
+	}
 
 	ngOnInit() {
+		this.checkLoginStatus();
+		this.navEnd.subscribe(() => {
+			this.checkLoginStatus();
+		});
+	}
+
+	private checkLoginStatus(): void {
 		this.isLoggedIn = this.auth.isLoggedIn();
 		if (this.isLoggedIn) {
 			this.user = this.auth.decodeToken();
+		}
+		else {
+			this.user = null;
 		}
 		console.log('inside ngOnInit() of Header, isLoggedIn:', this.isLoggedIn, ', userDetails:', this.user);
 	}
