67383729dc42e3f20659afa715268659f3bafa1b
5aledmaged <5aledmaged@gmail.com>
2018-10-07 17:18:43 +0200

    new HttpOptions interface for notes service

--- src/app/shared/note.ts
+++ src/app/shared/note.ts
@@ -1,5 +1,11 @@
-export class Note {
+import { HttpHeaders } from '@angular/common/http';
+
+export interface Note {
 	_id?: string;
 	title: string;
 	body: string;
+}
+
+export interface HttpOptions {
+	headers?: HttpHeaders;
 }
