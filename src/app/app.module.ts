c0ae08c1f887f129b3af5a6d71819e2f663496cb
5aledmaged <5aledmaged@gmail.com>
2019-10-11 14:59:13 +0200

    updated to angular material 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,12 @@ import { httpInterceptorProviders } from './interceptors/index';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
-import {
-	MatToolbarModule,
-	MatIconModule,
-	MatButtonModule,
-	MatCardModule,
-	MatInputModule,
-	MatMenuModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
