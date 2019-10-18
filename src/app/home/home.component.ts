e3d22edbf9afa3db2d70ac38c4680efde8f3edcc
5aledmaged <5aledmaged@gmail.com>
2019-10-18 17:10:37 +0200

    remove authentication logic from home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,20 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from '../auth.service';
 
 @Component({
 	templateUrl: './home.component.html',
 	styleUrls: ['./home.component.sass']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-	constructor(private router: Router, private auth: AuthService) { }
-
-	ngOnInit() {
-		const isLoggedIn = this.auth.isLoggedIn();
-		if (isLoggedIn) {
-			this.router.navigate(['/notes']);
-		}
-	}
+	constructor() { }
 
 }
