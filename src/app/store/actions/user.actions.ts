305e116ee8b93cc92dc07ac1374b681f3745d5c1
5aledmaged <5aledmaged@gmail.com>
2019-11-02 14:36:51 +0200

    add user login, user login success or fail actions

--- src/app/store/actions/user.actions.ts
+++ src/app/store/actions/user.actions.ts
@@ -2,3 +2,7 @@ import { createAction, props } from '@ngrx/store';
 import { User } from '../../entities/User';
 
 export const setActiveUser = createAction('[User] Set Active User', props<{ user: User }>());
+
+export const userLogin = createAction('[User] login', props<User>());
+export const userLoginSuccess = createAction('[User] login success', props<User>());
+export const userLoginFail = createAction('[User] login fail', props<Error>());
