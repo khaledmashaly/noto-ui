be7d5d596f472572e08866359a4b51f43f6bf588
5aledmaged <5aledmaged@gmail.com>
2019-12-11 20:29:04 +0200

    add note update to NoteEditor component

--- src/app/components/note/note-editor/note-editor.component.ts
+++ src/app/components/note/note-editor/note-editor.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { NoteService } from '../../../services/note.service';
 import { Note } from 'src/app/shared/note';
-import { catchError } from 'rxjs/operators';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { NoteEditorType } from './note-editor-type.enum';
+import { switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Component({
 	selector: 'app-note-editor',
@@ -10,6 +13,7 @@ import { catchError } from 'rxjs/operators';
 	styleUrls: ['./note-editor.component.sass']
 })
 export class NoteEditorComponent implements OnInit {
+	editorType: NoteEditorType;
 	noteForm = this.fb.group({
 		title: ['Title', Validators.required],
 		body: ['body']
@@ -17,10 +21,48 @@ export class NoteEditorComponent implements OnInit {
 
 	constructor(
 		private fb: FormBuilder,
+		private route: ActivatedRoute,
 		private noteService: NoteService
 	) { }
 
 	ngOnInit() {
+		this.route.data.subscribe(
+			(data) => this.editorType = data.editorType
+		);
+		this.route.paramMap.pipe(
+			switchMap(
+				(params: ParamMap) => {
+					if (this.editorType === NoteEditorType.Create) {
+						return of({
+							title: '',
+							body: ''
+						} as Note);
+					}
+					if (this.editorType === NoteEditorType.View) {
+						const noteID = params.get('id');
+						return this.noteService.getNote(noteID);
+					}
+					throw new Error('ValueError: editorType is not defined');
+				}
+			)
+		).subscribe(
+			(note) => {
+				this.title.setValue(note.title);
+				this.body.setValue(note.body);
+			},
+			(error) => {
+				console.log('note-editor, getNote error:', error);
+			}
+		);
+	}
+
+	saveNote() {
+		if (this.editorType === NoteEditorType.Create) {
+			return this.createNote();
+		}
+		if (this.editorType === NoteEditorType.View) {
+			return this.updateNote();
+		}
 	}
 
 	createNote() {
@@ -29,7 +71,17 @@ export class NoteEditorComponent implements OnInit {
 			body: this.body.value
 		};
 		this.noteService.createNote(note)
-			.subscribe(console.log);
+			.subscribe(console.log, console.error);
+	}
+
+	updateNote() {
+		const note: Note = {
+			_id: this.route.snapshot.paramMap.get('id'),
+			title: this.title.value,
+			body: this.body.value
+		};
+		this.noteService.editNote(note)
+			.subscribe(console.log, console.error);
 	}
 
 	get title() { return this.noteForm.get('title'); }
