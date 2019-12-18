b5e96599f29476f22d8633e051b1ab716035a810
5aledmaged <5aledmaged@gmail.com>
2019-12-18 13:41:44 +0200

    create a route name for all routes

---
+++ src/app/modules/routing/routes-names.ts
@@ -0,0 +1,16 @@
+export const ROUTE_NAMES = {
+	/* user routes */
+	userProfile: '/profile',
+	userLogin: '/login',
+	userRegister: '/register',
+	userHome: '/note/list',
+
+	/* note routes */
+	noteList: '/note/list',
+	noteCreate: '/note',
+	noteView: '/note',
+	noteEdit: '/note',
+
+	/* general routes */
+	appHome: '/'
+};
