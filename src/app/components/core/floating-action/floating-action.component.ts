697932bbaa020707208ce6d4249cd019db82cafb
5aledmaged <5aledmaged@gmail.com>
2019-12-21 14:19:08 +0200

    adjust some layout css

--- src/app/components/core/floating-action/floating-action.component.ts
+++ src/app/components/core/floating-action/floating-action.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ROUTE_NAMES } from 'src/app/modules/routing/routes-names';
 
 @Component({
 	selector: 'app-floating-action',
 	templateUrl: './floating-action.component.html',
 	styleUrls: ['./floating-action.component.sass']
 })
-export class FloatingActionComponent implements OnInit {
+export class FloatingActionComponent {
+	routes = ROUTE_NAMES;
 
-	constructor() { }
+	constructor(
+		private router: Router
+	) { }
 
-	ngOnInit() {
+	doAction() {
+		this.router.navigate([this.routes.noteCreate]);
 	}
-
 }
