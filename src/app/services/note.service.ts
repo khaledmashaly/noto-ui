406d2ac3ea9a5c6b890fdde9733d5d94f74f09ea
5aledmaged <5aledmaged@gmail.com>
2019-12-22 16:38:27 +0200

    move Note to entities

--- src/app/services/note.service.ts
+++ src/app/services/note.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Note } from '../shared/note';
+import { Note } from '../entities/Note';
 
 @Injectable({
 	providedIn: 'root'
